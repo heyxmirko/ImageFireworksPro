24ff12e27a2eb93750c66120299d1510d681e5d5
heyxmirko <lukasektomas1@seznam.cz>
2024-04-29 07:50:08 +0200

    Initial Commit

---
+++ src/main/java/me/lukyn76/imagefireworkspro/ImageFireworksPro.java
@@ -0,0 +1,17 @@
+package me.lukyn76.imagefireworkspro;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ImageFireworksPro extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
